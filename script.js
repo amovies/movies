f93c592ce8cd99dd20904d8c3e12f7872daad0d0
amovies <143316146+amovies@users.noreply.github.com>
2023-09-05 21:17:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const movies = [
     // { title: "", releaseDate: "2023-08-25", genre: "Action", languge: "Kannada",  downloadLink: "" },//
 
-    { title: "Sapta Sagaradaache Ello - Side A (2023) ", releaseDate: "2023-09-01", genre: "Action", languge: "Kannada",  downloadLink: "https://uploady.io/5q6mwfcfjcg4/Sapta-Sagaradaache-Ello---Side-A---40-2023--41--Kannada-HQ-PreDVD---720p---x264---HQ-Clean-Aud---1.4GB.mkv.mp4" },
+    { title: "Gadar 2 (2023) Hindi", releaseDate: "2023-08-11", genre: "Action", languge: "Hindi",  downloadLink: "https://uploady.io/aesuu2u5yqgd/Gadar-2---40-2023--41--Hindi-HQ-PreDVD---700MB---x264---MP3.mkv" },
+    { title: "Kushi (2023) Kannada", releaseDate: "2023-09-01", genre: "Action", languge: "Kannada",  downloadLink: "https://uploady.io/i8vvf57x98e8/Kushi---40-2023--41--Kannada-HQ-PreDVD---720p---x264---HQ-Clean-Aud---1.4GB.mkv" },
+    { title: "Sapta Sagaradaache Ello - Side A (2023) ", releaseDate: "2023-09-01", genre: "Action", languge: "Kannada",  downloadLink: "https://uploady.io/5q6mwfcfjcg4/Sapta-Sagaradaache-Ello---Side-A---40-2023--41--Kannada-HQ-PreDVD---720p---x264---HQ-Clean-Aud---1.4GB.mkv" },
     { title: "Vedha (2022) ", releaseDate: "2022-12-23", genre: "Action", languge: "Kannada",  downloadLink: "https://uploady.io/bj3rsq2r8xmc/Vedha---40-2022--41--HQ-HDRip---720p---HEVC-----40-AAC-2.0--41---Tam--Tel--Kan--Mal----900MB---ESub.mkv" },
     { title: "Toby kannada", releaseDate: "2023-08-25", genre: "Action", languge: "Kannada",  downloadLink: "https://uploady.io/djli84qbf0hk/Toby---40-2023--41--Kannada-HQ-PreDVD---720p---x264---HQ-Clean-Aud---1.6GB.mkv.mp4" },
    // { title: "", releaseDate: "2023-08-25", genre: "Action", languge: "Kannada",  downloadLink: "" },//
